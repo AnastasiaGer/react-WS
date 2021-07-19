9f71d0d4108f4f71cc5990fab96537f9a4f75e7d
AnastasiaGer <aagermanova@gmail.com>
2021-07-18 17:51:13 -0700

    s

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import {getFunName} from './../helpers.js'
+import React from "react";
+import { getFunName } from "./../helpers.js";
 class StorePicker extends React.Component {
   render() {
-    return ( 
-    <form action = "" className = 'store-selector' >
-      <h2> Please Enter A Store</h2>
-import {getFunName} from './../helpers.js'
-      <input type="text" required placeholder='Store name' defaultValue={getFunName()}/>
-      <button type="submit">Visit Store</button>
+    return (
+      <form action="" className="store-selector">
+        <h2> Please Enter A Store</h2>
+        <input
+          type="text"
+          required
+          placeholder="Store name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store</button>
       </form>
-    )
+    );
   }
 }
 
